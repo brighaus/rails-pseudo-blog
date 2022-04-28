ca1c082a8741f4e1ea9702944477f0ca40796935
Brigham Hausman <brig@bsoahc.org>
2022-04-27 18:30:40 -0600

    Starting CRUD from scratch, articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220401232810) do
+ActiveRecord::Schema.define(version: 20220428001715) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
 end
